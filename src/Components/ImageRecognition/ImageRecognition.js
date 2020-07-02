f6adf4df2c3584165bccbe6b2f5de8e2868757e3
Subham Chowdhury <sc761851@gmail.com>
2020-07-02 14:57:49 -0700

    image box added,

---
+++ src/Components/ImageRecognition/ImageRecognition.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageRecognition = ({imageUrl}) =>{
+return(
+    <div>
+        <img className="" src={imageUrl} alt="" width="500px" height="500px" />
+    </div>
+)
+
+}
+
+export default ImageRecognition;
