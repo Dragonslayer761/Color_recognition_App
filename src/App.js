a61baf4c609b9447d9c61b4f832c6ecbbf657542
Subham Chowdhury <sc761851@gmail.com>
2020-07-02 14:59:00 -0700

    functionality add for button and input field

--- src/App.js
+++ src/App.js
@@ -2,27 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from'./Components/Navigation/Navigation';
 import ImageLinkFrom from './Components/ImageLinkFrom/ImageLinkFrom';
+import ImageRecognition from './Components/ImageRecognition/ImageRecognition'
 import RankComponent from './Components/RankComponent/RankComponent';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '559d8ffe80834b4f8174c46fedd4c2d9'
+ });
 
 class App extends Component{
   constructor(){
     super();
     this.state={
-      inpuut:'',
+      input : '',
+      imageurl : ''
     }
   }
   onInputChange=(event)=>{
-    console.log(event.target.value);
+   this.setState({input:event.target.value});
   }
+   setImageUrl = ()=>{
+     this.setState({imageurl:this.state.input});
+   }
   onSubmit=()=>{
-    console.log('click');
+    this.setImageUrl();
+    app.models.predict("eeed0b6733a644cea07cf4c60f87ebb7", this.state.input).then(
+    function(response) {
+      let colors=response.rawData.outputs[0].data.colors;
+      for(let i of colors){
+        console.log(i.w3c.name);
+      }
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
   render(){
     return(
       <div className="App">
         <Navigation/>
         <RankComponent/>
-        <ImageLinkFrom onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
+        <ImageLinkFrom onInputChange={this.onInputChange} onSubmit={this.onSubmit} /> 
+        <ImageRecognition imageUrl={this.state.imageurl} />
       </div>
     )
   }
--- src/Components/ImageLinkFrom/ImageLinkFrom.js
+++ src/Components/ImageLinkFrom/ImageLinkFrom.js
@@ -7,7 +7,7 @@ const ImageLinkFrom = ({onInputChange,onSubmit}) => {
                 {'This magic brain will detect faces from images'}
             </p>
             <div className="pa1 center">
-                <input type="text" name="" id="" className="f4 pa2 w-70 mr1" onChange={onInputChange} />
+                <input type="text" name="" id="" className="f4 pa2 w-70 mr1" onInput={onInputChange} />
                 <button className="w30 grow f4 link ph3 pv2 dibwhite bg-light-purple" onClick={onSubmit}>Detect</button>
             </div>
 
